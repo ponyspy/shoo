fe3b08534da8e893220c089f1e4df34938385bbd
ponyspy <desade4me@gmail.com>
2021-04-13 20:56:33 +0300

    fix

--- apps/shoo/routes/main/news.js
+++ apps/shoo/routes/main/news.js
@@ -16,10 +16,23 @@ module.exports = function(Model) {
 	module.news = function(req, res, next) {
 		var id = req.params.news_id;
 
+		var get_locale = function(option, lg) {
+			return ((option.filter(function(locale) {
+				return locale.lg == lg;
+			})[0] || {}).value || '');
+		};
+
 		News.findOne({'$or': [{ '_short_id': id }, { 'sym': id }]}).where('status').ne('hidden').exec(function(err, news) {
 			if (err) return next(err);
 
-			res.render('main/news/news.pug', {news: news, moment: moment});
+			News.aggregate().match({
+				'_id': {'$ne': news._id},
+				'status': {'$ne': 'hidden'}
+				}).sample(2).exec(function(err, sim_news) {
+				if (err) return next(err);
+
+				res.render('main/news/news.pug', {get_locale: get_locale, news: news, sim_news: sim_news, moment: moment});
+			});
 		});
 	};
 
