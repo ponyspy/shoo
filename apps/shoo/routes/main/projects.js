2ada785f8d851373703d302d485f74b1b64adb10
ponyspy <desade4me@gmail.com>
2021-05-11 19:10:14 +0300

    log

--- apps/shoo/routes/main/projects.js
+++ apps/shoo/routes/main/projects.js
@@ -38,7 +38,7 @@ module.exports = function(Model) {
 
 			var query = category ? {'type': req.params.type, 'category': category._id} : {'type': req.params.type };
 			console.log(query)
-			console.log(req.body.context.category)
+			console.log(req.body.context.category || 'zlo')
 
 			Project.find(query).where('status').ne('hidden').sort('-build_date').skip(skip).limit(limit).populate('category').exec(function(err, projects) {
 				if (err) return next(err);
